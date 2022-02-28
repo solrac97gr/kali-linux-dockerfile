a790f08f6f368e833c406fbe79f25333efadf171
carlos97gr <carlosgperu97@gmail.com>
2022-02-28 17:00:17 +0300

    fist version

---
+++ README.md
@@ -0,0 +1,4 @@
+# Kali Linux Docker Image
+
+Execute the Docker Image using the script
+<code>./run.sh</code>
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo mkdir -p $HOME/Documents/Development/personal/kali-linux/volume && docker-compose run kali-light
